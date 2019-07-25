bc6e30a76c646b3c89513b32aed7e46fbadeae2a
Apollo Tang <tang.apollo@gmail.com>
2019-07-25 12:43:53 -0400

    01-react-basic add template of folder

---
+++ 01-react-basic/template/folder-template/00.js
@@ -0,0 +1,11 @@
+
+class MyApp extends React.Component {
+  render() {
+    return(
+      <div>hello</div>
+    )
+  }
+}
+
+const reactContainer = document.getElementById('react-container');
+ReactDOM.render(<MyApp />, reactContainer);
